bb59cec0ff820b98c55dfcc1117148c8ed976721
junhyeongchae <jchae7184@gmail.com>
2024-03-01 23:49:27 +0900

    next app router folder setting

---
+++ src/app/(beforeLogin)/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+import Image from "next/image";
+import zLogo from "../../../public/zlogo.png";
+import Link from "next/link";
+import Main from "./_component/Main";
+
+export default function Home() {
+  return <Main />;
+}
